3af7debe13c2d333db6fb5a1042090f1e43318a2
Syed Fasiuddin <syedfasiuddin.02@gmail.com>
2023-01-17 20:52:56 +0530

    feat: routes skeletion

--- src/index.ts
+++ src/index.ts
@@ -14,10 +14,25 @@ app.get("/", (_req: Request, res: Response): void => {
     res.sendFile("public/index.html");
 })
 
+app.post("/register", async (_req: Request, res: Response): Promise<void> => {
+    res.status(200);
+    res.end();
+})
+
+app.post("/login", async (_req: Request, res: Response): Promise<void> => {
+    res.status(200);
+    res.end();
+})
+
 app.post("/api", async (_req: Request, res: Response): Promise<void> => {
     res.status(200);
     const ID = await nanoid();
     res.send({id: ID});
+})
+
+app.get("/:short_url", async (_req: Request, res: Response): Promise<void> => {
+    res.status(200);
+    res.end();
 })
 
 app.listen(8000, () => {
